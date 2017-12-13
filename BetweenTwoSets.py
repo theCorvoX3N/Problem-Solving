3d7f55c16d941478a0799726f9632caa3124ec95
SHRI-SitaRam <a.noob.hacker@gmail.com>
2017-12-13 14:31:08 +0530

    used lamda

---
+++ BetweenTwoSets.py
@@ -0,0 +1,20 @@
+# HackerRank
+#  - Algorithms > Implementation > Between Two Sets
+# 13/12/17
+
+(n ,m) = map(int, input().strip().split(" "))
+a = list(map(int, input().strip().split(" ")))
+b = list(map(int, input().strip().split(" ")))
+
+x = max(a)
+i = 0
+count = 0
+
+while x <= min(b):
+    if sum(list(map(lambda y : x%y==0, a))) == len(a) and sum(list(map(lambda y: y%x ==0, b))) == len(b):
+        count += 1
+    else:
+        pass
+    x += 1
+
+print(count)
