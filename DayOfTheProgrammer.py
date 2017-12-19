eee61be379ef80de8793c880b006a9d697d213d2
SHRI-SitaRam <a.noob.hacker@gmail.com>
2017-12-19 22:34:47 +0530

    remove reduntant logic

---
+++ DayOfTheProgrammer.py
@@ -0,0 +1,19 @@
+# HackerRank
+#  - Algorithms > Implementation > Day of the Programmer
+#  19/12/17
+
+y  = int(input())
+
+if (y<1918 and y %4 ==0) or (y>1918 and (y % 400 == 0 or (y % 4 == 0 and not (y % 100 == 0)))):
+    print("12.09.%d" %y)
+
+elif y == 1918:
+    print("26.09.1918")
+
+else:
+    print("13.09.%d" % y)
+
+
+
+
+
